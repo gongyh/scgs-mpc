3239fecc8f01004a951f530aefa231ea0177fa26
gongyh <gongyh@qibebt.ac.cn>
2023-06-27 16:07:32 +0800

    generate tsv

--- generateHashFiles.sh
+++ generateHashFiles.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
+cat combinations/hash.tsv | while read line; do
 
+  IFS=$'\t' read -a ROW <<< "$line"
+  header=$'#targets\tbase_image\timage_build'
+  if [ "${ROW[0]}" != "#targets" ]; then
+    conda=${ROW[0]}
+    echo "processing ${conda}"
+    version=${ROW[2]}
+    hash2=$(echo $(mulled-hash $conda))
+    fname="${hash2}-${version}.tsv"
+    echo -e "$header" > pull/${fname}
+    echo "${ROW[*]}" | tr " " "\t" >> pull/${fname}
+  else
+    echo "header line: ${ROW[*]}"
+  fi
+
+done
+
+echo "Done!"
